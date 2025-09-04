ddacfee5db49c84bd127dbe3b5fa3697150c037b
MukZro <117829052+tuaanh03@users.noreply.github.com>
2025-09-04 03:26:33 +0000

    làm bt lab1

---
+++ lab1/bt1_test.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int findMax(int num1, int num2, int num3) {
+    int max = 0;
+    if ((num1 > num2) && (num1 > num3))
+        max = num1;
+    if ((num2 > num1) && (num2 > num3))
+        max = num2;
+    if ((num3 > num1) && (num3 > num2))
+        max = num3;
+    return max;
+}
+
+int main()
+{
+    int n1, n2, n3;
+
+    cin >> n1 >> n2 >> n3;
+
+    int result = findMax(n1,n2,n3);
+
+    cout<<result;
+
+    return 0;
+}
