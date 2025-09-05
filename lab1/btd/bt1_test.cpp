bbd96762c626bbbd201dc82042ec1e47d01d46ff
MukZro <117829052+tuaanh03@users.noreply.github.com>
2025-09-05 16:30:09 +0000

    continue done

---
+++ lab1/btd/bt1_test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int f3(int x) {
+if (log(x * x * cos(x)) < 3 * x) return 2 * x;
+else
+return 2 * x;
+}
+
+int main()
+{
+    int x ;
+    cin >> x;
+    cout << f3(x);
+    return 0;
+}
+
