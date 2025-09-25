ed79a4d602f4e490b79e2d0ca047e3789425e1ae
MukZro <117829052+tuaanh03@users.noreply.github.com>
2025-09-25 07:33:11 +0000

    update more 2

--- lab3/sgu25_ktpm-lab03_cpp_tests/src/lib/math_lib.cpp
+++ lab3/sgu25_ktpm-lab03_cpp_tests/src/lib/math_lib.cpp
@@ -14,7 +14,7 @@ int findMax(int num1, int num2, int num3) {
         max = num1;
     if ((num2 == num3) && (num2 > num1))
         max = num2;
-    // if ((num1 == num2) && (num1 == num3))
-    //     max = num1;
+    if ((num1 == num2) && (num1 == num3))
+        max = num1;
     return max;
 }
