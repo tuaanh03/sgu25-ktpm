5fdc704f902d20cb857fa9ff91dd8d7c2edbb3c9
MukZro <117829052+tuaanh03@users.noreply.github.com>
2025-09-25 03:58:35 +0000

    update

--- lab3/sgu25_ktpm-lab03_cpp_tests/src/app.cpp
+++ lab3/sgu25_ktpm-lab03_cpp_tests/src/app.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     cout << "Hello World" << endl;
     cout << "This is a test C++ application." << endl;
+    cout << "It doesn't do much, but it's here to demonstrate a simple C++ program." << endl;
     return 0;
 }
